c40b138726bcbf4173b3daa66ffc6d67d5399206
Jeffruy Examene <Examene7007@gmail.com>
2016-04-17 05:34:45 +0000

    Adding detail page for grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -2,28 +2,36 @@ class GramsController < ApplicationController
 	before_action :authenticate_user!, only: [:new, :create]
 	
 	def new
-	 @gram = Gram.new 
+	   @gram = Gram.new 
+	end
+
+	def show
+	  # show grams using params id
+	  @gram = Gram.find_by_id(params[:id])
+	  if @gram.blank?
+	   render text: "Not Found :(", status: :not_found
+	  end
 	end
 		
 	def index
 	end
 
 	def create
-		# create a gram uisng gram_params and current user id
-		@gram = current_user.grams.create(gram_params)
-		#check to if gram valid, if is send to root path, else unprocessable entity
-		if @gram.valid?
-		 redirect_to root_path
-		else
-		 render :new, status: :unprocessable_entity
-		end
+	  # create a gram uisng gram_params and current user id
+	  @gram = current_user.grams.create(gram_params)
+	  #check to if gram valid, if is send to root path, else unprocessable entity
+	  if @gram.valid?
+		redirect_to root_path
+	  else
+	    render :new, status: :unprocessable_entity
+	  end
 	end
 
 	private
 	
 	def gram_params
-		# params for gram require a gram, permit message
-		params.require(:gram).permit(:message)
+	  # params for gram require a gram, permit message
+	  params.require(:gram).permit(:message)
 	end
 
 
